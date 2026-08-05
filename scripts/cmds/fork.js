d25cf0a0eb20b858d24c937e32530aba37a32046
AhmeD’s shi'shir <saju01430@gmail.com>
2026-08-05 18:03:59 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -14,18 +14,18 @@ module.exports = {
 
   langs: {
     en: {
-      current: "╭━━━〔 𝐆𝐎𝐀𝐓-𝐁𝐎𝐓-𝐕𝟐 〕━━━⦿\n┃\n┃ 👑 𝗢𝘄𝗻𝗲𝗿: 𝖷𝖺𝗅𝗆𝖺𝗇 𝖧𝗈𝗌𝗌𝖺𝗂𝗇\n┃ 🔗 𝗥𝗲𝗽𝗼: %1\n┃ 💎 𝗦𝘁𝗮𝘁𝘂𝘀: always updating\n┃\n┃ ❝ 𝖳𝗁𝖾 𝖦𝗋𝖾𝖺𝗍𝖾𝗌𝗍 𝖮𝖿 𝖠𝗅𝗅 𝖳𝗂𝗆𝖾 𝖠𝗎𝗍𝗈𝗆𝖺𝗍𝗂𝗈𝗇 ❞\n┃\n╰━━━━━━〔 📥 〕━━━━━━⦿"
+      current: "╭━━━〔 𝚂𝙷𝙸𝚂𝙷𝙸𝚁-𝐁𝐎𝐓-𝐕5 〕━━━⦿\n┃\n┃ 👑 𝗢𝘄𝗻𝗲𝗿: 𝑨𝒉𝒎𝒆𝑫’𝒔 𝑺𝒉𝒊'𝒔𝒉𝒊𝒓\n┃ 🔗 𝗥𝗲𝗽𝗼: %1\n┃ 💎 𝗦𝘁𝗮𝘁𝘂𝘀: always updating\n┃\n┃ ❝ 𝖳𝗁𝖾 𝖦𝗋𝖾𝖺𝗍𝖾𝗌𝗍 𝖮𝖿 𝖠𝗅𝗅 𝖳𝗂𝗆𝖾 𝖠𝗎𝗍𝗈𝗆𝖺𝗍𝗂𝗈𝗇 ❞\n┃\n╰━━━━━━〔 📥 〕━━━━━━⦿"
     }
   },
 
   onStart: async function ({ message, getLang }) {
-    const link = "https://github.com/goatbotnx/GOAT-BOT-V2";
+    const link = "আমার শিশির বস এর প্যান্টের নিচে ফর্ক আছে। যদি লাগে তাহলে শিশির বসের টুনটুনি থাকা টু টাকা কইলাম না। হক পেয়ে যাবা 😂";
     return message.reply(getLang("current", link));
   },
 
   onChat: async function ({ message, getLang, event }) {
     if (event.body && event.body.toLowerCase() === "fork") {
-      const link = "https://github.com/goatbotnx/GOAT-BOT-V2";
+      const link = "আমার শিশির বস এর প্যান্টের নিচে ফর্ক আছে যা কাছে ফক দিয়ে দিব নে 😂";
       return message.reply(getLang("current", link));
     }
   }
