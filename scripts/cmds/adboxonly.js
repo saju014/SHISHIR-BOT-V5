68ba57d30bb00a3d635e3f4522596dc258577514
AhmeD’s shi'shir <saju01430@gmail.com>
2026-08-05 17:07:18 +0600

    Update adboxonly.js

--- scripts/cmds/adboxonly.js
+++ scripts/cmds/adboxonly.js
@@ -3,7 +3,7 @@ module.exports = {
 		name: "onlyadminbox",
 		aliases: ["onlyadbox", "adboxonly", "adminboxonly"],
 		version: "1.3",
-		author: "NTKhang",
+		author: "shishir",
 		countDown: 5,
 		role: 1,
 		description: {
