5453a29bf3fbf8e43f80c482658879e4e3929adb
AhmeD’s shi'shir <saju01430@gmail.com>
2026-08-13 11:49:22 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -9,7 +9,7 @@ module.exports = {
         version:     "1.2",
         author:      "Shishir",
         countDown:   5,
-        role:        0,
+        role:        2,
         description: "Change the bot's prefix or show current prefix.",
         category:    "config",
         guide: {
