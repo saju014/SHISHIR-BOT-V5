898112345577a35e8ca7da35d4d1a95ce99b56e1
AhmeD’s shi'shir <saju01430@gmail.com>
2026-08-13 11:58:13 +0600

    Change role from 0 to 2 in antiout.js

--- scripts/cmds/antiout.js
+++ scripts/cmds/antiout.js
@@ -4,7 +4,7 @@ module.exports = {
     version: "1.0",
     author: "AceGun",
     countDown: 5,
-    role: 0,
+    role: 2,
     shortDescription: "Enable or disable antiout",
     longDescription: "",
     category: "box chat",
